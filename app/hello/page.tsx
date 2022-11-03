2a06acbd49ed35e1ff49df056842079543cb5700
Travis DePrato <travis@mergequeue.com>
2022-11-03 13:33:57 -0700

    Repro

---
+++ app/hello/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import Image from "next/image";
+import vercel from "./vercel.client.svg"
+import wikimedia from "./wikimedia.client.png"
+
+export default function HelloPage() {
+    return (
+    <div>
+      <h2>Client side</h2>
+        <Image src={vercel} alt={"Vercel Client Import"}/>
+        <Image src={wikimedia} alt={"Wikimedia Server Import"}/>
+    </div>
+  );
+}
---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import vercel from "./vercel.server.svg";
+import wikimedia from "./wikimedia.server.png";
+
+
+export default function Layout({ children }: { children: React.ReactNode}) {
+    return <html lang="en">
+    <body>
+    <div>
+        <h2>Server Side</h2>
+        <Image src={vercel} alt="Vercel Server Import"/>
+        <Image src={wikimedia} alt="Wikimedia Server Import"/>
+    </div>
+    <div>{children}</div>
+    </body></html>
+}
